fcbb795d38fdbb706250562b61f012305817fe2e
Travis Williamson <travywill@gmail.com>
2019-01-15 15:27:42 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -39,7 +39,7 @@ class Student
     SQL
 
     DB[:conn].execute(sql, self.name, self.grade)
-    @id = DB[:conn].execute("SELECT last_insert_rowide() from students")[0][0]
+    #@id = DB[:conn].execute("SELECT last_insert_rowide() from students")[0][0]
   end
 
 end
